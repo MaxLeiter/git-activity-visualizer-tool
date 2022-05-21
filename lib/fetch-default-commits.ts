6bb8ae81e2ad115308a75cb51805285ded875e6a
Max Leiter <maxwell.leiter@gmail.com>
2022-05-20 20:29:33 -0700

    fix api function

--- lib/fetch-default-commits.ts
+++ lib/fetch-default-commits.ts
@@ -1,9 +1,16 @@
 import { Commits } from "pages/api/fetch-github-events"
 
+// in case we break the deployed version, we can use this...
+const NEED_TO_FIX = true
+
 const fetchDefaultCommits = async () => {
-    const API_URL = process.env.NODE_ENV === "production" ? 
+    let API_URL = process.env.NODE_ENV === "production" ? 
         "https://git-visualizer.maxleiter.com/api/fetch-github-events?username=MaxLeiter" :
         "http://localhost:3000/api/fetch-github-events?username=MaxLeiter"
+
+    if (NEED_TO_FIX) {
+        API_URL = "https//hours-visualization-fv2ds610z-max-leiter.vercel.app/api/fetch-github-events?username=MaxLeiter"
+    }
 
     const commitsByRepo = await fetch(API_URL)
 
